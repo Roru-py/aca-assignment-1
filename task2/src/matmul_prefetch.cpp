ce411a082892c745ce16726947c99f09221ceee6
Rohit <rohitrajkumar.1810@gmail.com>
2026-09-04 18:59:38 +0530

    task2 partb

--- task2/src/matmul_prefetch.cpp
+++ task2/src/matmul_prefetch.cpp
@@ -6,7 +6,5 @@
 
 void matmul_prefetch(const float* A, const float* B, float* C,
                      int M, int N, int K, int lda, int ldb, int ldc) {
-    // TODO(student): replace this placeholder with your cache-blocked SIMD + prefetch
-    // implementation.
-    matmul_naive(A, B, C, M, N, K, lda, ldb, ldc);
+    
 }
